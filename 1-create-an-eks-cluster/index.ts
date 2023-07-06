6b043a7406b9b039facd5845b2765319ae3722c7
Yifan Xu <xuyifan02@pingcap.com>
2023-07-06 15:25:13 +0800

    fix lint

--- 1-create-an-eks-cluster/index.ts
+++ 1-create-an-eks-cluster/index.ts
@@ -12,39 +12,50 @@ const cluster = new eks.Cluster('my-eks', {
 })
 
 // Install the EBS CSI driver on the cluster.
-const clusterOidcProviderUrl = cluster.core.oidcProvider!.url
-const clusterOidcProviderArn = cluster.core.oidcProvider!.arn
+const clusterOidcProviderUrl = cluster.core.oidcProvider!.url // eslint-disable-line
+const clusterOidcProviderArn = cluster.core.oidcProvider!.arn // eslint-disable-line
 const ebsCsiDriverName = 'my-eks-ebs-csi-driver'
-const role = pulumi.all([clusterOidcProviderUrl, clusterOidcProviderArn]).apply(([url, arn]) =>
-    new aws.iam.Role(ebsCsiDriverName, {
-        assumeRolePolicy: {
-            Version: "2012-10-17",
-            Statement: [{
-                Effect: "Allow",
-                Principal: {
-                    Federated: arn
-                },
-                Action: "sts:AssumeRoleWithWebIdentity",
-                Condition: {
-                    StringEquals: {
-                        [`${url.replace('https://', '')}:aud`]: "sts.amazonaws.com",
-                        [`${url.replace('https://', '')}:sub`]: `system:serviceaccount:kube-system:ebs-csi-controller-sa`,
+const role = pulumi.all([clusterOidcProviderUrl, clusterOidcProviderArn]).apply(
+    ([url, arn]) =>
+        new aws.iam.Role(ebsCsiDriverName, {
+            assumeRolePolicy: {
+                Version: '2012-10-17',
+                Statement: [
+                    {
+                        Effect: 'Allow',
+                        Principal: {
+                            Federated: arn
+                        },
+                        Action: 'sts:AssumeRoleWithWebIdentity',
+                        Condition: {
+                            StringEquals: {
+                                [`${url.replace('https://', '')}:aud`]:
+                                    'sts.amazonaws.com',
+                                [`${url.replace(
+                                    'https://',
+                                    ''
+                                )}:sub`]: `system:serviceaccount:kube-system:ebs-csi-controller-sa`
+                            }
+                        }
                     }
-                }
-            }]
-        },
-    })
+                ]
+            }
+        })
 )
-const attachment = role.apply(r => new aws.iam.RolePolicyAttachment(ebsCsiDriverName, {
-    role: r,
-    policyArn: aws.iam.ManagedPolicy.AmazonEBSCSIDriverPolicy,
-}))
-const addon = role.apply(r =>
-    new aws.eks.Addon(ebsCsiDriverName, {
-        addonName: "aws-ebs-csi-driver",
-        clusterName: cluster.core.cluster.name,
-        serviceAccountRoleArn: r.arn,
-    })
+const attachment = role.apply(
+    (r) =>
+        new aws.iam.RolePolicyAttachment(ebsCsiDriverName, {
+            role: r,
+            policyArn: aws.iam.ManagedPolicy.AmazonEBSCSIDriverPolicy
+        })
+)
+const addon = role.apply(
+    (r) =>
+        new aws.eks.Addon(ebsCsiDriverName, {
+            addonName: 'aws-ebs-csi-driver',
+            clusterName: cluster.core.cluster.name,
+            serviceAccountRoleArn: r.arn
+        })
 )
 
 // Export the cluster's kubeconfig so that we can access kubeconfig outside this pulumi stack.
--- 2-deploy-tidb-with-tidb-operator/index.ts
+++ 2-deploy-tidb-with-tidb-operator/index.ts
@@ -1,54 +1,59 @@
-import * as pulumi from "@pulumi/pulumi";
-import * as k8s from "@pulumi/kubernetes";
+import * as pulumi from '@pulumi/pulumi'
+import * as k8s from '@pulumi/kubernetes'
 
 // Use the EKS cluster from the previous step.
-const eksCluster = new pulumi.StackReference("organization/1-create-an-eks-cluster/default");
-const eksProvider = new k8s.Provider("eks-provider",
-    {
-        kubeconfig: eksCluster.getOutput("kubeconfig"),
-        enableServerSideApply: true,
-    }
-);
+const eksCluster = new pulumi.StackReference(
+    'organization/1-create-an-eks-cluster/default'
+)
+const eksProvider = new k8s.Provider('eks-provider', {
+    kubeconfig: eksCluster.getOutput('kubeconfig'),
+    enableServerSideApply: true
+})
 
 // Install the TiDB Operator CRDs.
-const tidbOperatorCRDs = new k8s.yaml.ConfigGroup("tidb-operator-crds",
-    {files: "crds/*.yaml"},
-    {provider: eksProvider},
-);
+const tidbOperatorCRDs = new k8s.yaml.ConfigGroup(
+    'tidb-operator-crds',
+    { files: 'crds/*.yaml' },
+    { provider: eksProvider }
+)
 
 // Deploy the TiDB Operator.
-const tidbOperator = new k8s.helm.v3.Release("tidb-operator", {
+const tidbOperator = new k8s.helm.v3.Release(
+    'tidb-operator',
+    {
         repositoryOpts: {
-            repo: "https://charts.pingcap.org/",
+            repo: 'https://charts.pingcap.org/'
         },
-        chart: "tidb-operator",
-        version: "v1.4.4",
+        chart: 'tidb-operator',
+        version: 'v1.4.4',
         values: {
             scheduler: {
-                kubeSchedulerImageName: "registry.k8s.io/kube-scheduler",
+                kubeSchedulerImageName: 'registry.k8s.io/kube-scheduler'
             }
         }
     },
-    {provider: eksProvider, dependsOn: [tidbOperatorCRDs]},
-);
-
+    { provider: eksProvider, dependsOn: [tidbOperatorCRDs] }
+)
 
 // Install the TiDB cluster.
-const tidbCluster = new k8s.yaml.ConfigGroup("tidb-cluster",
+const tidbCluster = new k8s.yaml.ConfigGroup(
+    'tidb-cluster',
     {
-        files: "tidb-cluster-manifests/*.yaml",
+        files: 'tidb-cluster-manifests/*.yaml',
         transformations: [
             // Put every resource in the created namespace.
-            (obj: any) => {
-                if (obj.metadata !== undefined) {
-                    obj.metadata.annotations = {"pulumi.com/patchForce": "true"}
+            (obj: any) => {                                               // eslint-disable-line
+                if (obj.metadata !== undefined) {                               // eslint-disable-line
+                    obj.metadata.annotations = {                                // eslint-disable-line
+                        'pulumi.com/patchForce': 'true'
+                    }
                 } else {
-                    obj.metadata = {
-                        annotations: {"pulumi.com/patchForce": "true"}
+                    obj.metadata = {                                            // eslint-disable-line
+                        annotations: { 'pulumi.com/patchForce': 'true' }
                     }
                 }
             }
-        ],
+        ]
     },
-    {provider: eksProvider, dependsOn: [tidbOperator]},
-);
+    { provider: eksProvider, dependsOn: [tidbOperator] }
+)
